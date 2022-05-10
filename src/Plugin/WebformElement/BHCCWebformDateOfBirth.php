cff4eaaf097fbe9cf7614ca5217dc1e5f3dbff69
Rosie Catchpole <rosie.catchpole@brighton-hove.gov.uk>
2022-05-10 16:42:37 +0100

    Added docroot/modules/custom/bhcc_webform_date/

---
+++ src/Plugin/WebformElement/BHCCWebformDateOfBirth.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\bhcc_webform_date\Plugin\WebformElement;
+
+use Drupal\webform\Plugin\WebformElement\WebformCompositeBase;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\bhcc_webform_date\Plugin\WebformElement\BHCCWebformDate;
+
+/**
+ * Provides a 'bhcc_webform_date_of_birth' element.
+ * 
+ * @WebformElement(
+ *   id = "bhcc_webform_date_of_birth",
+ *   label = @Translation("BHCC Date Of Birth"),
+ *   description = @Translation("Provides a webform element example."),
+ *   category = @Translation("Composite elements"),
+ *   multiline = TRUE,
+ *   composite = TRUE,
+ *   states_wrapper = TRUE,
+ * )
+ * 
+ * @see \Drupal\webform_example_composite\Element\WebformExampleComposite
+ * @see \Drupal\webform\Plugin\WebformElement\WebformCompositeBase
+ * @see \Drupal\webform\Plugin\WebformElementBase
+ * @see \Drupal\webform\Plugin\WebformElementInterface
+ * @see \Drupal\webform\Annotation\WebformElement
+ */
+class BHCCWebformDateOfBirth extends BHCCWebformDate {
+
+}
