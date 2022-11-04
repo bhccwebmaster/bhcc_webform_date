a6a5873ea2ae27254c92a888d6c0a5c73870b1fb
Alvin J. Alexander <rosie.catchpole@brighton-hove.gov.uk>
2022-11-04 16:58:42 +0000

    And another js attempt

--- js/date_element.js
+++ js/date_element.js
@@ -7,40 +7,27 @@
    */
   function hideErrorsOnElement(indvElement) {
     var indvElementWrapper = indvElement.find('.js-form-item');
-    var indvElementWrapperDiv = indvElementWrapper.find('.div');
-    //indvElementWrapper.addClass('visually-hidden');
-    indvElementWrapper.removeClass('has-error');
-    indvElement.find('input + .invalid-feedback').remove();
-
-    
+    var dateElementErrorMessages = indvElementWrapper.find('div.form-item--error-message');
+    // indvElementWrapper.removeClass('has-error');
+    // indvElement.find('input + .invalid-feedback').remove();
     // get all the sub date elements showing an error
-    // var subElements = indvElementWrapperDiv.getElementsByClassName("form-required");
-    // var thumbLi = $('.form-required').find('div');
-    //var thumbLi = $('.form-required.form-item--error-message');
-    // var thumbLi = $('.form-required').find('.form-item--error-message');
-    // var thumbLi = $('.form-required div');
-    // var thumbLi = $('label.form-required');
+    //var dateElementErrorMessages = $('div.form-item--error-message');
+    // skip the 1st as this is the parent date element
+    for (i = 0; i < dateElementErrorMessages.length; i++) {
+      dateElementErrorMessages.eq(i).addClass('visually-hidden');
+    }
+  }
+/*     var indvElementWrapper = indvElement.find('.js-form-item');
+    var indvElementWrapperDiv = indvElementWrapper.find('.div');
+    // indvElementWrapper.removeClass('has-error');
+    // indvElement.find('input + .invalid-feedback').remove();
+    // get all the sub date elements showing an error
     var dateElementErrorMessages = $('div.form-item--error-message');
     // skip the 1st as this is the parent date element
     for (i = 1; i <= dateElementErrorMessages.length; i++) {
       dateElementErrorMessages.eq(i).addClass('visually-hidden');
     }
-/*     for (var i = 0; i < subElements.length; i++) {
-      //hide the error message;
-      var div = subElements[i].addClass("visually-hidden");
-    }  */   
-    
-/*     subElementslist = indvElementWrapperDiv.getElementsByClassName("form-required");
-    for (let i = 0; i < subElementslist.length; i++) {
-      subElementslist[i].each(function(index, element) {
-        $(element).addClass("odd");
-      });
-    }  */
-
-/*     $('.form-required').each(function(index, element) {
-      $(element).addClass("odd");
-    });    */
-  }
+  } */
 
   Drupal.behaviors.bhcc_webform_date = {
     attach: function(context, settings) {
