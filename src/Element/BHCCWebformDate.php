596e4424b03810aead966cd1710c0651c730d813
Rosie Catchpole <rosie.catchpole@brighton-hove.gov.uk>
2023-04-19 16:42:23 +0100

    Add validation to change date error display
    so from (Y-m-d 2023-03-17) to d-m-Y (17-03-2923)

--- src/Element/BHCCWebformDate.php
+++ src/Element/BHCCWebformDate.php
@@ -11,6 +11,9 @@ use Drupal\webform\Element\WebformCompositeBase;
 use Drupal\webform\Utility\WebformArrayHelper;
 use Drupal\Core\Datetime\DateHelper; */
 use Drupal\localgov_forms_date\Element\LocalgovFormsDate;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Component\Utility\NestedArray;
 
 /**
  * Provides a 'bhcc_webform_date'.
@@ -42,6 +45,105 @@ class BHCCWebformDate extends LocalgovFormsDate {
     ];
     $returnInfo = array_replace($parentInfo, $childInfo);
     return $returnInfo;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function validateDatelist(&$element, FormStateInterface $form_state, &$complete_form) {
+
+    // dpm($element);
+    $value = NestedArray::getValue($form_state->getValues(), $element['#parents']);
+    $element_key = end($element['#parents']);
+
+    // Only process if it is of type
+    // Bhcc_webform_date or bhcc_webform_date_of_birth
+    if ($element['#type'] != "bhcc_webform_date" && $element['#type'] != "bhcc_webform_date_of_birth") {
+      return;
+    }
+    parent::validateDatelist($element, $form_state, $complete_form);
+    
+/*     if ($form_state->getErrors()) {
+      //$form_state->clearErrors();
+      foreach ($form_state->getErrors() as $error_key => $error_value) {
+        // if (strpos($error_value, " must be on or ") == 0) {
+        if ($error_value instanceof TranslatableMarkup) {
+
+          // if arguments are null then this has already been dealt with
+          if (NULL != $error_value->getArguments()) {
+
+            $date_placeholder = $error_value->getArguments();
+            if (isset($date_placeholder['%min']) || isset($date_placeholder['%max'])) {
+
+              if (isset($date_placeholder['%min'])){
+                $date = $date_placeholder['%min'];
+                $error_string = " must be on or after ";
+              }  
+              else {
+                $date = $date_placeholder['%max'];
+                $error_string = " must be on or before ";
+              }
+              
+              $element_name = $date_placeholder['%name'];
+              // Put in 2003-10-16 format
+              $date = date('d-m-Y', strtotime($date));
+              unset($error_value);
+              $form_state->setErrorByName($error_key, $element_name . $error_string . $date);
+            }
+            else {
+              // this handles required fields where argument 
+              // '%field' is used add it back in 
+              $form_state->setErrorByName($error_key, $error_value);
+            }
+          }
+        } 
+        else {
+          //add back in all other errors
+          $form_state->setErrorByName($error_key, $error_value);
+        }
+      }
+    } */
+
+    $form_errors = $form_state->getErrors();
+    if ($form_errors) {
+      $form_state->clearErrors();
+      foreach ($form_errors as $error_key => $error_value) {
+        if ($error_value instanceof TranslatableMarkup) {
+
+          // if arguments are null then this has already been dealt with
+          if (NULL != $error_value->getArguments()) {
+
+            $date_placeholder = $error_value->getArguments();
+            if (isset($date_placeholder['%min']) || isset($date_placeholder['%max'])) {
+
+              if (isset($date_placeholder['%min'])){
+                $date = $date_placeholder['%min'];
+                $error_string = " must be on or after ";
+              }  
+              else {
+                $date = $date_placeholder['%max'];
+                $error_string = " must be on or before ";
+              }
+              
+              $element_name = $date_placeholder['%name'];
+              // Put in 2003-10-16 format
+              $date = date('d-m-Y', strtotime($date));
+              unset($error_value);
+              $form_state->setErrorByName($error_key, $element_name . $error_string . $date);
+            }
+            else {
+              // this handles required fields where argument 
+              // '%field' is used add it back in 
+              $form_state->setErrorByName($error_key, $error_value);
+            }
+          }
+        } 
+        else {
+          //add back in all other errors
+          $form_state->setErrorByName($error_key, $error_value);
+        }
+      }
+    }
   }
 
 }
